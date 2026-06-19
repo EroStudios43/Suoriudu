812d62cda35a04598adf4e62289ae2317536fb76
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2026-06-19 18:37:07 +0300

    Made a responsive UI for student home page using bootstrap according to the Figma-diagram. Still needs the box and functionality for searching / adding courses as a student. The percent for showing the amount of done exercises not implemented yet.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -59,12 +59,15 @@ export default function Home() {
         if (selected.getTime() === yesterday.getTime()) {
             return `Eilen  ${formattedDate}`;
         }
-        return date.toLocaleDateString("fi-FI", {
+        
+        const lowercaseDate = date.toLocaleDateString("fi-FI", {
             weekday: "long",
             year: "numeric",
             month: "long",
             day: "numeric",
         });
+
+        return String(lowercaseDate).charAt(0).toUpperCase() + String(lowercaseDate).slice(1)
     };
 
     useEffect(() => {
@@ -100,20 +103,96 @@ export default function Home() {
     getCourses();
 }, [user, refresh]);
 
-    
+  if (user.role === "teacher"){
+    return (
+        <div className="home-container">
+            <div className="topbar">
+                <h1 className="welcome-text">
+                    Hei {user.firstname} {user.lastname}!
+                </h1>       
 
-  return (
-    <div className="home-container">
+                <div className="topbar-right-icons">
+                    <button className="icon-button" onClick={e => navigate("/TaskQuestions")}>
+                        <i className="fa-solid fa-envelope"></i>
+                    </button>
+
+                    <button className="icon-button" onClick={e => navigate("/Profile")}>
+                        <i className="fa-solid fa-circle-user"></i>
+                    </button>
+
+                    <button className="icon-button" onClick={e => logout()}>
+                        <i className="fa-solid fa-sign-out-alt"></i>
+                    </button>
+
+                </div>
+            </div>
+
+            <div className="divider"></div>
+            <p className="info-text">
+                Tehtävät ja kokeet 
+            </p>
+
+            <div className="calendar-content">
+                
+                <div className="left-side">
+                            
+                    <div className="info-card">
+                        <h2>{formatDate(selectedDate)}</h2>
+                        <p>Tänne tulee myöhemmin backendistä tietoa.</p>
+                    </div>
+
+                    <button className="marathon-btn" onClick={e => navigate("/TaskEvaluation")}>
+                            <p>Arviointimaratoni</p>
+                            <p className="info-marathon">
+                                Arvioi anonyymisti opiskelijoiden tehtäviä satunnaisessa järjestyksessä 
+                                valitsemaltasi kurssilta!
+                            </p>
+                    </button>
+                </div>
+
+                <div className="right-side">
+                    <Calendar selectedDate={selectedDate} onDateSelect={setSelectedDate}/>
+                </div>
+            </div>
+
+            <div className="courses-header">
+                <h1>Kurssit</h1>
+
+                <button className="create-course-btn" onClick={e => navigate("/CreateCourse")}>
+                    <i class="fa-solid fa-plus"></i>
+                </button>
+            </div>
+
+            <div className="courses-content">
+                {courses && courses.length > 0 ? (
+                    courses.map((course, index) => (
+                        <div
+                            key={index}
+                            className={`course-card course-color-${index % 4}`}
+                            >
+                            <h2>{course.coursename}</h2>
+                            <button className="course-arrow" onClick={() => navigate(`/CoursePage/${course.idcourse}`)}>
+                                <i class="fa-regular fa-circle-right arrow-icon"></i>
+                            </button>
+                        </div>
+                    ))
+                ) : (
+                    <p className="no-courses">Et ole luonut vielä yhtään kurssia</p>
+                )}
+
+            </div>
+
+        </div>
+    );
+  } else if (user.role === "student") {
+    return(
+    <div className="container-fluid home-container">
         <div className="topbar">
             <h1 className="welcome-text">
                 Hei {user.firstname} {user.lastname}!
             </h1>       
 
             <div className="topbar-right-icons">
-                <button className="icon-button" onClick={e => navigate("/TaskQuestions")}>
-                    <i className="fa-solid fa-envelope"></i>
-                </button>
-
                 <button className="icon-button" onClick={e => navigate("/Profile")}>
                     <i className="fa-solid fa-circle-user"></i>
                 </button>
@@ -126,61 +205,112 @@ export default function Home() {
         </div>
 
         <div className="divider"></div>
+
         <p className="info-text">
             Tehtävät ja kokeet 
         </p>
 
-        <div className="calendar-content">
+        <div className="row">
             
-            <div className="left-side">
-                           
+            {/* Today's exercises box */}
+            <div className="col-md m-2">   
+                <h2>{formatDate(new Date())}</h2>    
                 <div className="info-card">
-                    <h2>{formatDate(selectedDate)}</h2>
                     <p>Tänne tulee myöhemmin backendistä tietoa.</p>
                 </div>
-
-                <button className="marathon-btn" onClick={e => navigate("/TaskEvaluation")}>
-                        <p>Arviointimaratoni</p>
-                        <p className="info-marathon">
-                            Arvioi anonyymisti opiskelijoiden tehtäviä satunnaisessa järjestyksessä 
-                            valitsemaltasi kurssilta!
-                        </p>
-                </button>
             </div>
 
-            <div className="right-side">
+            {/* Tomorrow's exercises box */}
+            <div className="col-md m-2">
+                <h2>
+                    { /* Getting tomorrow's date in order for the formatDate function to return
+                     the right information. If the date is changed from the calendar, 
+                     change it to that date. */}
+
+                    {formatDate(
+                        (() => {
+                        const d = new Date();
+                        if (d.getDate() === selectedDate.getDate()) {
+                            d.setDate(d.getDate() + 1);
+                            return d;
+                        } else {
+                            return selectedDate
+                        }
+                        })()
+                    )}
+                </h2>  
+                <div className="info-card">
+                    <p>Tänne tulee myöhemmin backendistä tietoa.</p>
+                </div>
+            </div>
+
+            {/* The calendar component */}
+            <div className="col-md">
+                <h2>Kalenteri</h2>
                 <Calendar selectedDate={selectedDate} onDateSelect={setSelectedDate}/>
+                <br />
+                
+                <h2>Tulossa</h2>
+                <div className="info-card">
+                    <p>Tähän tulee myöhemmin backendistä tietoa</p>
+                    <br />
+                    <br />
+                    <br />
+                </div>
             </div>
         </div>
 
-        <div className="courses-header">
-            <h1>Kurssit</h1>
+        { /* The courses section */}
+        <div className="divider"></div>
 
-            <button className="create-course-btn" onClick={e => navigate("/CreateCourse")}>
-                <i class="fa-solid fa-plus"></i>
-            </button>
+        <div className="row">
+            <div className="col-6 col-sm-7 col-md-9"><h1>Kurssit</h1></div> 
+            <div className="col-6 col-sm-5 col-md-3 student-add-courses-input align-self-center">
+                <form>
+                    <div className="row">
+                        <div className="col-2 col-sm-1 align-self-center">
+                            <i className="fa-solid fa-bars"></i>
+                        </div>
+                        <div className="col-8 col-sm-9 align-self-center">
+                            <input 
+                                id="courseSearch"
+                                name="courseSearch"
+                                className="form-control"
+                                placeholder="Lisää kursseja"
+                            />
+                        </div>
+                        <div className="col-2 col-sm-1 align-self-center">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
 
-        <div className="courses-content">
+        {/* Bring all the courses the student is part of here. */}  
+        {/* If the course is not dividable by 2, make a new row */}
+        <div className="row justify-content-evenly">
             {courses && courses.length > 0 ? (
                 courses.map((course, index) => (
                     <div
                         key={index}
-                        className={`course-card course-color-${index % 4}`}
+                        className={`course-card-student col-md-5 m-2 course-color-${index % 4}`}
+                        onClick={() => navigate(`/CoursePage/${course.idcourse}`)}
                         >
-                        <h2>{course.coursename}</h2>
-                        <button className="course-arrow" onClick={() => navigate(`/CoursePage/${course.idcourse}`)}>
-                            <i class="fa-regular fa-circle-right arrow-icon"></i>
-                        </button>
+                        <div className="text-truncate">
+                            <h2>{course.coursename}</h2>
+                            <p>{course.course_description}</p>
+                        </div>
                     </div>
                 ))
             ) : (
-                <p className="no-courses">et ole luonut vielä yhtään kurssia</p>
+                <p className="no-courses">Et ole liittynyt vielä yhdellekään kurssille.</p>
             )}
 
         </div>
 
     </div>
-  );
+    );
+  }
 }
 
